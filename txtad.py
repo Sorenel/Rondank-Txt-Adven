3688b8dbba6971acfb4d46dffc290d1d2fdcfa61
Ronstan <ronnystanu@gmail.com>
2016-10-24 12:59:32 +1100

    Add files via upload

---
+++ txtad.py
@@ -0,0 +1,188 @@
+import os
+clear = lambda: os.system('cls')
+import msvcrt as m
+
+def textadventure():
+    print("Hello! Welcome to Ronstan's text adventure!\nThere are a few rules to fully enjoy this game.\nPress any key to read the next line.\nWhen prompted for a response, refrain from using captital letters.\nThank You and Enjoy!")
+    print("")
+    username = input("Enter your username to continue: ")
+    clear()
+    print("A warm welcome, " + username + ".")
+    m.getch()
+    print("")
+    print("The world you are about to enter is one of greed and glory,")
+    m.getch()
+    print("one of chivalry companionship, ")
+    m.getch()
+    print("a world different to the democratic regime of the 21st century.")
+    m.getch()
+    print("")
+    print("Welcome to Easthaven")
+    print("")
+    print("Press any key to continue...")
+    m.getch()
+    clear()
+
+    #begin story draft 2
+    print("It was hazy.")
+    m.getch()
+    print("")
+    print("You could barely make out anything apart from the shadows of tall trees\nwhich had been corrupted by the forest.")
+    m.getch()
+    print("")
+    print("You have no idea why you are here, and shattered memories of a former life\nswirl inside your head.")
+    m.getch()
+    print("")
+    print("Assortments of blades, collections of wares:")
+    m.getch()
+    print("")
+    print("They all flash through your mind, although you can't make any sense of it")
+    print("")
+    print("")
+    print("Press any key to continue...")
+    m.getch()
+    clear()
+    print("Suddenly, there is this impending feeling of fear, and the instinct to run")
+    m.getch()
+    print("")
+    print("You don't know what is chasing you- IF something is chasing you.")
+    m.getch()
+    print("")
+    print("The thought is crushed instantaneously in your mind as you focus\non what is ahead.")
+    m.getch()
+    print("")
+    print("You find your way out of all the trees and discover a pathway.")
+    m.getch()
+    print("")
+    print("You have no clue where it leads, but what else are you meant to do?")
+    m.getch()
+    print("")
+    print("You walk along the pathway for what seems like hours, your ragged flesh\ncontinuously making contact with the black, tainted bricks.")
+    print("")
+    print("")
+    print("Press any key to continue...")
+    m.getch()
+    clear()
+    print("Proceeding down this seemlessy never-ending path is tiresome,")
+    m.getch()
+    print("")
+    print("nothing seems to spike your interest and you feel like giving up.")
+    m.getch()
+    print("")
+    print("You reach a fork in the path which leads left or right")
+    m.getch()
+    print("")
+    print("In front of you lies a sign:")
+    m.getch()
+    print("")
+    print("The sign reads 'The left path shall yeild rewards if thou seeks,\nand the right path shalt be easier, at the cost of none.")
+    m.getch()
+    print("")
+    userchoice1 = input("Which path will you take? left or right?")
+    m.getch()
+    clear()
+    if(userchoice1 == "left"):
+        dagger = True
+        print("You decide to take the left path.")
+        m.getch()
+        print("")
+        print("The road seems quite similiar to the path you just trod.")
+        m.getch()
+        print("")
+        print("You make your way continuously foward until a sound breaks the silence")
+        m.getch()
+        print("")
+        print("The sound gets louder and louder as you progress further until\nyou can make out that it is a man's voice")
+        m.getch()
+        print("")
+        print("")
+        print("Press any key to continue...")
+        m.getch()
+        clear()
+        print("You progress further until you see a ghoulish figure on the ground admist it's  shaken surroundings.")
+        m.getch()
+        print("")
+        print("It was obvious that there was some kind of action that led to the corpse,\nthe trees appeared as if they had been smited and the earth had a morbid tone ofblack.")
+        m.getch()
+        print("")
+        print("As you inspect the body you discover a Bloodstained Shiv; obviously used in some\nsort of ritual.")
+        m.getch()
+        print("")
+        print("You see the body start the twitch followed by a series of heavy breaths emitting from the body.")
+        m.getch()
+        clear()
+        print("You take the Bloodstained Shiv and run.")
+        print("")
+        m.getch()
+        clear()
+        print("You put the painful throbbing of your sore muscles aside and continue along\nthe beaten path until you see a sign standing amidst the overgrowth.")
+        print("")
+        m.getch()
+        print("                                  'Welcome to Ebonstorm'  ")
+        m.getch()
+        clear()
+        #START OF EBONSTORM
+        print("You proceed ahead of the sign straight past the gateway into the hustle and\nbustle of Ebonstorm's streets")
+        print("")
+        m.getch()
+        print("The streets are paved with cobblestone, and the locals look as friendly as ever.")
+        print("")
+        m.getch()
+        print("One of them beckons you with a friendly gesture to examine his wares.")
+        userchoice2 = input("Will you examine his wares(y) or wander elsewhere(n)?")
+        if(userchoice2 == "y"):
+            market = 1
+            print("")
+            m.getch()
+            print("You take a look at the display of weapons; spears, swords and bows litter his marketplace.")
+            print("")
+            m.getch()
+            print("
+              
+     
+        
+        
+    elif(userchoice1 == "right"):
+        dagger = False
+        print("You decide to take the right path.")
+        print("")
+        m.getch()
+        print("Aside from the intermittent tree branch, the path remained quite drab though wearisome")
+        print("")
+        m.getch()
+        print("You quickly reach the end of the path, marked by a sign that read:")
+        m.getch()
+        print("                                  'Welcome to Ebonstorm'  ")
+        m.getch()
+        clear()
+        #START OF EBONSTORM
+        print("You proceed ahead of the sign straight past the gateway into the hustle and\nbustle of Ebonstorm's streets")
+        print("")
+        m.getch()
+        print("The streets are paved with cobblestone, and the locals look as friendly as ever")
+        print("")
+        m.getch()
+        print("One of them beckons you with a friendly gesture to examine his wares")
+        print("")
+        
+        
+    else:
+        print("The input you entered was no understood, please enter either left or right.")
+        
+    
+    
+    
+    
+          
+    
+    
+          
+
+
+
+
+
+
+
+textadventure()
+    
